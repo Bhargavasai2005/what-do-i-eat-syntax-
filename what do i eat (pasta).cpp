4e63618336b2a2427e3ad3997c12449026f03c50
Bhargavasai2005 <94349388+Bhargavasai2005@users.noreply.github.com>
2021-11-20 23:00:53 +0530

    Add files via upload

---
+++ what do i eat (pasta).cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+main()
+{
+	//1.display the menu
+	printf("pick an item:\n1.pizza\n2.burger\n3.pasta\n4.french fries\n5.sandwich");
+	//2.READ their choice
+	int choice=0;
+	scanff("%d",&choice);
+	//3.display based on their choice
+	
+	switch(condition)
+	{
+		case1:
+			printf("YOU picked pizza");
+			break;
+		case2:
+			printf("YOU picked burger");
+			break;
+		case3:
+			printf("YOU picked pasta");
+			break;
+		case4:
+			printf("YOU picked french fries");
+			break;
+		case5:
+			printf("YOU picked sandwich");
+			break;
+		deafualt:printf("ivalind choice");
+	} 
